d68c58edf574628b75ba373d0763b2778bc3c45b
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-14 06:01:48 +0200

    Added decodeURI method for slug

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,7 +1,9 @@
 export default function Project({ params }: { params: { slug: string } }) {
+    const decodedURI: string = decodeURI(params.slug);
+
     return (
         <>
-            {params.slug}
+            {decodedURI}
         </>
     )
 }
