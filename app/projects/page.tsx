39bc4fbdf5442799148462511974ede82b54d76c
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-14 05:03:24 +0200

    Added initial display of projects to Projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,6 +2,8 @@
 
 import { useEffect, useState } from "react";
 import { fetchData } from "./actions";
+import { Flex, Paper, Stack, UnstyledButton, Text, Badge } from "@mantine/core";
+import Link from "next/link";
 
 export default function Projects() {
     const [data, setData] = useState<DoD | undefined>(undefined);
@@ -15,11 +17,34 @@ export default function Projects() {
     }, []);
     return (
         <>
-            <div>
-                Projects page
-            </div>
             {data && (
-                <div>{JSON.stringify(data)  }</div>
+                <Stack
+                    align="stretch"
+                    justify="flex-start"
+                    gap="md"
+                >
+                    {data.releases.map((release: Release, index: number) => (
+                        <Paper shadow="sm" radius="lg" withBorder p="sm" key={index}>
+                            <Flex
+                                gap="md"
+                                justify="space-between"
+                                align="center"
+                                direction="row"
+                                wrap="wrap"
+                            >
+                                <div>
+                                    <UnstyledButton c="blue.5" fw={500} component={Link} href={'projects/' + release.name}>
+                                        {release.name}
+                                    </UnstyledButton>
+                                    &nbsp;{release.organization && (<Text span c="dimmed">&nbsp;{release.organization}</Text>)}
+                                </div>
+                                <div>
+                                    <Text span ff="monospace" c="dimmed">{release.version && (release.version + " - ")}</Text><Badge>{release.status}</Badge>
+                                </div>
+                            </Flex>
+                        </Paper>
+                    ))}
+                </Stack>
             )}
         </>
     )
