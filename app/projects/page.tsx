d7605334adf258c4a5b81876fce094e0400881ca
Nikola Stancic <88898574+Nikoraii@users.noreply.github.com>
2024-05-14 05:16:25 +0200

    Added pagination

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,11 +2,22 @@
 
 import { useEffect, useState } from "react";
 import { fetchData } from "./actions";
-import { Flex, Paper, Stack, UnstyledButton, Text, Badge } from "@mantine/core";
+import { Flex, Paper, Stack, UnstyledButton, Text, Badge, Pagination } from "@mantine/core";
 import Link from "next/link";
+import classes from './page.module.css';
+
+function chunk(releases: Release[] | [], size: number): any {
+    if (!releases.length) {
+      return [];
+    }
+    const head = releases.slice(0, size);
+    const tail = releases.slice(size);
+    return [head, ...chunk(tail, size)];
+}
 
 export default function Projects() {
     const [data, setData] = useState<DoD | undefined>(undefined);
+    const [activePage, setActivePage] = useState(1);
 
     useEffect(() => {
         fetchData()
@@ -16,36 +27,49 @@ export default function Projects() {
             });
     }, []);
 
+    const releasesPerPage = 10;
+    const paginatedData = chunk(data && data.releases ? data.releases : [], releasesPerPage);
+
+    const handlePageChange = (page: any) => {
+        setActivePage(page);
+    };
+
     return (
         <>
             {data && (
-                <Stack
-                    align="stretch"
-                    justify="flex-start"
-                    gap="md"
-                >
-                    {data.releases.map((release: Release, index: number) => (
-                        <Paper shadow="sm" radius="lg" withBorder p="sm" key={index}>
-                            <Flex
-                                gap="md"
-                                justify="space-between"
-                                align="center"
-                                direction="row"
-                                wrap="wrap"
-                            >
-                                <div>
-                                    <UnstyledButton c="blue.5" fw={500} component={Link} href={'projects/' + release.name}>
-                                        {release.name}
-                                    </UnstyledButton>
-                                    &nbsp;{release.organization && (<Text span c="dimmed">&nbsp;{release.organization}</Text>)}
-                                </div>
-                                <div>
-                                    <Text span ff="monospace" c="dimmed">{release.version && (release.version + " - ")}</Text><ColorBadge status={release.status} />
-                                </div>
-                            </Flex>
-                        </Paper>
-                    ))}
-                </Stack>
+                <>
+                    <Stack
+                        align="stretch"
+                        justify="flex-start"
+                        gap="md"
+                    >
+                        {paginatedData.length > 0 && paginatedData[activePage - 1].map((release: Release, index: number) => (
+                            <Paper shadow="sm" radius="lg" withBorder p="sm" key={index}>
+                                <Flex
+                                    gap="md"
+                                    justify="space-between"
+                                    align="center"
+                                    direction="row"
+                                    wrap="wrap"
+                                >
+                                    <div>
+                                        <UnstyledButton c="blue.5" fw={500} component={Link} href={'projects/' + release.name}>
+                                            {release.name}
+                                        </UnstyledButton>
+                                        &nbsp;{release.organization && (<Text span c="dimmed">&nbsp;{release.organization}</Text>)}
+                                    </div>
+                                    <div>
+                                        <Text span ff="monospace" c="dimmed">{release.version && (release.version + " - ")}</Text><ColorBadge status={release.status} />
+                                    </div>
+                                </Flex>
+                            </Paper>
+                        ))}
+                        {!paginatedData.length && (
+                            <div>NO DATA</div>
+                        )}
+                    </Stack>
+                    <Pagination classNames={{ root: classes.root }} ta='center' withEdges total={paginatedData.length} value={activePage} onChange={handlePageChange} mt="sm" />
+                </>
             )}
         </>
     )
